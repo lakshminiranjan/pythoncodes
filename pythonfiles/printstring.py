fc65227df6e0bd569846fd9e68c2466d1c21809f
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-05-29 16:32:08 +0600

    Add files via upload

---
+++ pythonfiles/printstring.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+#printing strings in different ways
+
+print("Hello World!")
+print('Hello World!')
+
+print("""This string runs
+multiple lines like we can write paragraphs over here""")
+
+print("This is use to conacatenante"+" two words,The + sign actually used to conacatenate")
