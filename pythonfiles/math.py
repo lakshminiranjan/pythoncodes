d05c8eed0bc3ea9a808348134ef410e7c147b965
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-05-29 16:58:08 +0600

    Add files via upload

---
+++ pythonfiles/math.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+#Maths
+print(50 + 50)#adding
+print(2 - 1)#subtracting
+print(2 * 2)#multiplying
+print(4 / 2)#dividing
+print(50 + 50 - 50 * 50 / 50)#PEMDAS(Peranthesis,Exponents,Multiplications,Division,Addition,Subtraction)
+print(2 ** 2)#exponents
+print(50 % 6)#modulo,Output is given like(50-48("6*8"))=2
+print(50 / 3)#dividing a number which is actually not divided by
+print(50 // 3)#converts the output into an integer
