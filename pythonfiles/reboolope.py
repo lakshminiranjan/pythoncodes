9e812794c30e36a811d996dc8fc2aa1d3f82c47f
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-05-31 10:18:07 +0600

    Add files via upload

---
+++ pythonfiles/reboolope.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+#Relational and boolean operators
+#Be good at truth table
+greater_than = 7 > 5
+less_than = 2 < 3
+greater_than_equal_to = 2 >= 2
+less_than_equal_to = 4 <=4
+
+#multiple operators
+
+test_and = (7 > 5) and (5 < 7) #True
+test_and1 = (2 > 1) and (2 < 1)#False(and we use so its false one is false whole statement is false)
+test_and2 = (7 > 5) or (5 < 7)#True
+test_and3 = (2 > 1) or (2 < 1)#True(only one can be true total statement is true)
+
+test_not = not True #False
