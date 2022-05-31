0ea69de1ee6d9f9de857052a49276d0c460f3ad0
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-05-31 10:04:10 +0600

    Add files via upload

---
+++ pythonfiles/booleanexp.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+#Boolean expressions(True or false)
+#Note:True-(Boolean) and "True"-(String)
+print('Boolean Expressions :')
+
+bool1 = True#Boolean
+bool2 = 3*3 == 9
+bool3 = False
+bool4 = 3*3 != 9
+
+print(bool1,bool2,bool3,bool4)
+print(type(bool1))
+
+
+bool5 = "True"#String
+print(type(bool5))
