29675d2d760e181e0fe5b9445e43990f4afdc43f
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-06-01 11:45:00 +0600

    Add files via upload

---
+++ pythonfiles/importing.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+#Importing is important.Some Modules in py are not built-in but available to us.
+import sys #sys is system functions and parameters.(Very importing)
+from datetime import datetime#just import the datetime as it is.
+from datetime import datetime as dt#import with alias
+print(dt.now())#prints the exact time and date of present
+print(datetime.now())#prints the exact time and date of present
+print(sys.version)#prints the python version.
