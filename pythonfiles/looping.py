03c93163aa43cf31ef2ff76dbdd98214b61d7f11
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-06-01 11:32:32 +0600

    Add files via upload

---
+++ pythonfiles/looping.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+#for loop is used when we know a starting and ending point for a given scenario.
+#while loop is used when we are not perfect with the ending point.
+#For Loop-start to finish of a "iterate".
+cars = ["BMW" ,"BenZ" ,"Audi" ,"ToYoTo"]
+for x in cars:
+	print(x)
+
+
+#While Loop - Executes as long as the statement is true.
+
+i = 1
+while i < 10:#Loop stops when i=10.
+	print(i)
+	i+=1#addding 1 to something 
