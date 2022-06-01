6981851e645f55fc88a4f4f4069324be7dd76bc3
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-06-01 11:22:51 +0600

    Add files via upload

---
+++ pythonfiles/tuples.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+#Tuples-are "IMUTABLE"-have peranthesis()
+grades = ("a" , "b" , "c" , "d" , "f" )
+print(grades[1])#prints the second element i.e., "b".
+print(grades[0 :])
+#Everything works similar to list the only change is instead of brackets we use peranthesis and cant append or pop a element.
