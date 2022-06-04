6e4462d9146eb686c65bbdd196bf6a91bd2368fa
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-06-04 20:33:46 +0600

    Add files via upload

---
+++ pythonfiles/newfunction.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+'''
+to concatenate ',' is major thing shouls be used and even we can use '+' but if we use '+" we have to convert everything into str again
+'''
+def fun(name):
+	print("Hi",name)
+fun('Nizy')
