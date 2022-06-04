30165d8f77ba2ea8050e8dba0ce7ff6a905668d4
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-06-04 19:22:53 +0600

    Add files via upload

---
+++ pythonfiles/appendfilesinpy/appendfiles.py
@@ -0,0 +1,4 @@
+#!/bin/python3
+#appending text in a python file.
+con_file = open("countries.txt",'a')#'a' indicates appending text to a file.
+con_file.write('\nThis is a new line')#prints this a new line in countries file
